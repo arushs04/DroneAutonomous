a7601527a420c60ad8ef4ffab49d8ff1cfce39bf
arushs04 <52867225+arushs04@users.noreply.github.com>
2022-03-30 15:24:41 -0400

    Add files via upload

---
+++ asn5_3.py
@@ -0,0 +1,52 @@
+from djitellopy import Tello
+
+
+def main():
+    droneStraight()
+
+
+def droneStraight():
+    tello = Tello()
+    tello.connect()
+    print(tello.get_battery())
+    tello.takeoff()
+
+    tello.move_forward(100)
+
+    tello.land()
+
+
+def droneSquareFacingForward():
+    tello = Tello()
+
+    tello.connect()
+    tello.takeoff()
+
+    tello.move_forward(100)
+    tello.move_left(100)
+    tello.move_back(100)
+    tello.move_right(100)
+
+    tello.land()
+
+
+def turningFunction(sides, distance):
+    tello = Tello()
+    angle = 360 // sides
+    tello.connect()
+    print(tello.get_battery())
+    tello.takeoff()
+
+    for i in range(sides):
+        tello.move_forward(distance)
+        tello.rotate_counter_clockwise(angle)
+
+    tello.land()
+
+
+def droneSquareTurning():
+    turningFunction(4, 100)
+
+
+if __name__ == "__main__":
+    main()
